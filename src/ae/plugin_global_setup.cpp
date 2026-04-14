4ea892b844716ca3efd7028576bf5bef220f2299
noeljustin <n.lent@gmx.de>
2026-04-14 23:07:54 +0200

    fix: avoid AE load-time pixel format suite acquisition

--- src/ae/plugin_global_setup.cpp
+++ src/ae/plugin_global_setup.cpp
@@ -1,10 +1,6 @@
 #include "plugin_entry.h"
 #include "plugin_version.h"
 
-#include "AEFX_SuiteHelper.h"
-#include "AE_EffectCBSuites.h"
-#include "AE_EffectPixelFormat.h"
-
 #include "ghost_cuda.h"
 
 #include <cstdio>
@@ -23,7 +19,7 @@ PF_Err PluginHandleAbout(PF_InData*, PF_OutData* out_data, PF_ParamDef*[], PF_La
 
 PF_Err PluginHandleGlobalSetup(PF_InData* in_data, PF_OutData* out_data, PF_ParamDef*[], PF_LayerDef*)
 {
-    PF_Err err = PF_Err_NONE;
+    (void)in_data;
     out_data->my_version = FLARESIM_AE_PIPL_VERSION;
 
     out_data->out_flags = PF_OutFlag_DEEP_COLOR_AWARE;
@@ -32,21 +28,6 @@ PF_Err PluginHandleGlobalSetup(PF_InData* in_data, PF_OutData* out_data, PF_Para
                            PF_OutFlag2_SUPPORTS_THREADED_RENDERING;
     if (cuda_ghost_renderer_compiled()) {
         out_data->out_flags2 |= PF_OutFlag2_SUPPORTS_GPU_RENDER_F32;
-
-        AEFX_SuiteScoper<PF_PixelFormatSuite2> pixel_format_suite(
-            in_data,
-            kPFPixelFormatSuite,
-            kPFPixelFormatSuiteVersion2,
-            out_data);
-        ERR(pixel_format_suite->PF_ClearSupportedPixelFormats(in_data->effect_ref));
-        ERR(pixel_format_suite->PF_AddSupportedPixelFormat(in_data->effect_ref,
-                                                           PF_PixelFormat_ARGB128));
-        ERR(pixel_format_suite->PF_AddSupportedPixelFormat(in_data->effect_ref,
-                                                           PF_PixelFormat_ARGB64));
-        ERR(pixel_format_suite->PF_AddSupportedPixelFormat(in_data->effect_ref,
-                                                           PF_PixelFormat_ARGB32));
-        ERR(pixel_format_suite->PF_AddSupportedPixelFormat(in_data->effect_ref,
-                                                           PF_PixelFormat_GPU_BGRA128));
     }
-    return err;
+    return PF_Err_NONE;
 }
